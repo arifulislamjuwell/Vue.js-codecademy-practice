f0bd7adc76636e25a22b203eb6e6d550dbcfe4af
juwel <juwelariful1@gmail.com>
2020-03-28 23:21:10 +0600

    class and attribute bind

--- vue practice/callback.js
+++ vue practice/callback.js
@@ -24,9 +24,20 @@ function print(val, callback){
 
 const app = new Vue({
     el: "#app",
-    
     data:{
-        name: 'juwel'
+        name: 'juwel',
+        web:{
+            link1:{
+                name:'facebook', title:"go to facebook", url:'https://www.facebook.com'
+            },
+            link2:{
+                name:'youtube', title:"go to youtube", url: 'www.youtube.com'
+            }
+        },
+        activelinks: 'link1'
+    },
+    mounted: function(){
+        console.log('vue done')
     }
 })
 console.log(app)
