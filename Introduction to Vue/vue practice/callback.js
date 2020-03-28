353bce5bd307d97a3aa8b60fa77f27cc3bf909af
juwel <juwelariful1@gmail.com>
2020-03-28 22:05:12 +0600

    vue practice folder created and practive data binding

---
+++ Introduction to Vue/vue practice/callback.js
@@ -0,0 +1,11 @@
+console.log('up')
+console.log('middle')
+console.log('down')
+
+
+function print(val){
+    setTimeout(function(){
+
+    }), 1000
+
+}
---
+++ vue practice/callback.js
@@ -0,0 +1,32 @@
+const jsoN = {
+    name: 'juwel',
+    web: 'juwel.com',
+    skill:['python','js','vue','django'],
+    friends:[
+        {name: 'diganta', web:'diganta.com'},
+        {name: 'anik', web:'anik.com'}
+    ]
+}
+console.log(jsoN.friends[1].web)
+console.log('up')
+print('middle', third)
+
+function third(){
+    console.log('down')
+}
+function print(val, callback){
+    setTimeout(function(){
+        console.log(val)
+        callback()
+    }, 2000)
+}
+
+
+const app = new Vue({
+    el: "#app",
+    
+    data:{
+        name: 'juwel'
+    }
+})
+console.log(app)
